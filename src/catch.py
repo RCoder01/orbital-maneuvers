a2208a87c4910afdf9852d40e781dcb21a9fbd8a
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-12-04 20:40:09 -0500

    Add debug info

--- src/catch.py
+++ src/catch.py
@@ -3,16 +3,31 @@ import lib
 import math
 import constants
 
+
+class Writer:
+    def __init__(self):
+        self.string = ''
+    
+    def write(self, s):
+        self.string += s
+
+
+
 def resources_to_transfer(
         object_1: dict,
         object_2: dict,
         time_offset: float = 0,
-        extra_budget: float = 0
+        extra_budget: float = 0,
+        debug_log = None
         ) -> tuple[float, float]:
+    if debug_log is None:
+        debug_log = Writer()
+
     orbit_1 = lib.Orbit2d.from_dict(object_1)
     orbit_2 = lib.Orbit2d.from_dict(object_2)
     orbit_i = find_intermediate_orbit(orbit_1, orbit_2)
     orbit_j = find_extra_budget_orbit(extra_budget, orbit_1, orbit_2)
+    print(f'Changing from {orbit_1.semimajor_axis/1000:.0f} km to {orbit_2.semimajor_axis/1000:.0f} km', file=debug_log)
 
 
     inc_1 = float(object_1['INCLINATION'])
@@ -26,6 +41,7 @@ def resources_to_transfer(
     RAAN_delta = RAAN_1 - RAAN_2
     if RAAN_delta < 0:
         RAAN_delta += 360
+    print(f'initial RAAN: {RAAN_1}, target RAAN: {RAAN_2}, delta: {RAAN_delta}', file=debug_log)
 
     time_to_precess_to_2 = partial(match_RAAN, target_precession_rate=nodal_precession_2, RAAN_delta=RAAN_delta)
 
@@ -36,6 +52,13 @@ def resources_to_transfer(
         (time_to_precess_to_2(orbit_i, inc_2), orbit_i.period),
         (j_precession_time, orbit_j.period),
         key=lambda x: x[0])
+
+    if period == orbit_1.period:
+        print(f'match RAAN at 1, {min_time_to_precess:.0f} s', file=debug_log)
+    if period == orbit_i.period:
+        print(f'match RAAN at i, {min_time_to_precess:.0f} s', file=debug_log)
+    if period == orbit_j.period:
+        print(f'match RAAN at j, {min_time_to_precess:.0f} s', file=debug_log)
 
     orbits_to_precess = min_time_to_precess / period
 
@@ -52,14 +75,23 @@ def resources_to_transfer(
         inc_change_opportunities.append(necessary_inc_change_dv(orbit_j))
     inc_change_dv = min(inc_change_opportunities)
 
+    print(f'inc change from {inc_1} to {inc_2} (delta {inc_delta}) requires {inc_change_dv} m/s', file=debug_log)
+    print(f'change inc at {["1", "2", "i", "j"][inc_change_opportunities.index(inc_change_dv)]}', file=debug_log)
+
     orbits_to_match_mean_anomaly = match_mean_anomaly(orbit_2, period, orbits_to_precess)
+    print(f'time to match mean anomaly: {orbits_to_match_mean_anomaly * period} s', file=debug_log)
 
     orbit_1_to_i = lib.tangential_orbit_change_dv(orbit_1, orbit_i)
     orbit_i_to_2 = lib.tangential_orbit_change_dv(orbit_i, orbit_2)
+    print(f'orbit_1: {orbit_1.semimajor_axis/1000:.0f} km', file=debug_log)
+    if j_used:
+        print(f'orbit_j: {orbit_j.semimajor_axis/1000:.0f} km (1 to j to 1: {extra_budget:.2f} m/s)', file=debug_log)
+    print(f'orbit_i: {orbit_i.semimajor_axis/1000:.0f} km (1 to i: {orbit_1_to_i:.2f} m/s)', file=debug_log)
+    print(f'orbit_2: {orbit_2.semimajor_axis/1000:.0f} km (i to 2: {orbit_i_to_2:.2f} m/s)', file=debug_log)
 
     total_dv = orbit_1_to_i + orbit_i_to_2 + inc_change_dv + j_change_dv_total
     total_time = orbits_to_precess * period + orbits_to_match_mean_anomaly * orbit_2.period
-    return total_dv, total_time
+    return total_dv, total_time, debug_log
 
 
 def match_RAAN(
@@ -136,13 +168,13 @@ def collect(
     metadata = []
     while v < total_fuel_budget:
         try:
-            catch, dv, dt, index = collect_one(objects, caught, start, per_catch_fuel_budget, per_catch_time_target, t)
+            catch, dv, dt, index, debug_info = collect_one(objects, caught, start, per_catch_fuel_budget, per_catch_time_target, t)
         except ValueError:
             break
         v += dv
         t += dt
         caught.append(catch)
-        metadata.append((dv, dt, index))
+        metadata.append((dv, dt, index, debug_info))
     return caught, v, t, metadata
 
 
@@ -158,9 +190,9 @@ def collect_one(
         index += 1
         if objects[index] in caught:
             continue
-        dv, dt = resources_to_transfer(caught[-1], objects[index], current_time, per_catch_fuel_budget)
+        dv, dt, debug_info = resources_to_transfer(caught[-1], objects[index], current_time, per_catch_fuel_budget)
         if dt < per_catch_time_target:
-            return objects[index], dv, dt, index
+            return objects[index], dv, dt, index, debug_info
     raise ValueError('No object found')
 
 
@@ -178,18 +210,19 @@ if __name__ == '__main__':
 
     import sys
 
-    PER_CATCH_FUEL_BUDGET = 300 # m/s
-    PER_CATCH_TIME_BUDGET = 10**7.25 # s
-    TOTAL_FUEL_BUDGET = 5000 # m/s
+    PER_CATCH_FUEL_BUDGET = 100 # m/s
+    PER_CATCH_TIME_BUDGET = 10**7.7 # s
+    TOTAL_FUEL_BUDGET = 1100 # m/s
     START = int(sys.argv[1])
 
     caught, v, t, meta = collect(s_objects, START, PER_CATCH_FUEL_BUDGET, PER_CATCH_TIME_BUDGET, TOTAL_FUEL_BUDGET)
 
-    for dv, dt, i in meta:
+    for dv, dt, i, debug_info in meta:
         print(f'{i:3}: {dv:3.0f} m/s, 10^{math.log10(dt):4.2f} s')
+        print(debug_info.string)
     print(f'cumulative ({len(caught) - 1}): {v:3.0f} m/s, {t:.0f} s (10^{math.log10(t):4.2f} s, {t/60/60/24/365:5.2f} years)')
 
     print(f'deorbit dv: {deorbit_dv(lib.Orbit2d.from_dict(caught[-1])):.2f} m/s')
 
-    for _, _, i in [(0, 0, START)] + meta:
-        print(f'{i}: {s_objects[i]["OBJECT_ID"]:13} {s_objects[i]["OBJECT_NAME"]}')
+    # for _, _, i, _ in [(0, 0, START, 0)] + meta:
+    #     print(f'{i}: {s_objects[i]["OBJECT_ID"]:13} {s_objects[i]["OBJECT_NAME"]}')
