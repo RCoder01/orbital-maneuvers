d5d338cc4baa0ae16a5edd701c46ecff9032a7f2
RCoder01 <45217840+RCoder01@users.noreply.github.com>
2022-12-13 01:04:06 -0500

    Add file handling to graph_inclinations.py

--- src/graph_inclinations.py
+++ src/graph_inclinations.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-def graph():
+def graph(incs: list[float]):
     plt.hist(incs, bins=tuple(range(145)))
     plt.title('Debris in Low Earth Orbit by Inclination', fontsize=12)
     plt.xlabel('Inclination')
@@ -10,8 +10,31 @@ def graph():
     plt.savefig(r'graphs\leo_inclinations.png', bbox_inches='tight', dpi=300)
     plt.clf()
 
+def create_incs(input_filename: str):
+    import json
+    with open(input_filename, mode='r', encoding='UTF-8') as f:
+        objects = json.load(f)
+    incs = list(map(lambda o: o['INCLINATION'], objects))
+    with open('../data/incs.txt', mode='w', encoding='UTF-8') as f:
+        for inc in incs:
+            f.write(f'{inc}\n')
+
 if __name__ == '__main__':
-    with open('../data/incs.txt', mode='r', encoding='UTF-8') as f:
-        incs = f.readlines()
+    import sys
+    if len(sys.argv) > 1:
+        try:
+            create_incs(sys.argv[1])
+        except FileNotFoundError:
+            print(f'File with name {sys.argv[1]} not found')
+            quit()
+
+    try:
+        with open('../data/incs.txt', mode='r', encoding='UTF-8') as f:
+            global incs
+            incs = f.readlines()
+    except FileNotFoundError:
+        print('File with name ../data/incs.txt not found, '
+        'try running this script with a debris file name as an argument')
+        quit()
     incs = list(map(lambda s: float(s.strip()), incs))
-    graph()
+    graph(incs)
